3d5c361da919e173dd901b51b39b6629f81b7805
YOUR NAME <YOUR@EMAIL.com>
2018-04-12 17:07:22 +0700

    Chap7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 module ApplicationHelper
-  def full_title(page_title = "")
+  def full_title page_title = ""
     base_title = I18n.t "static_pages.base_title"
-    if page_title.empty?
-      base_title
-    else
-      page_title + " | " + base_title
-    end
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
