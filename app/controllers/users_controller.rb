72d04c2f4121cc2a99e0b09b1b11abc35174b09f
YOUR NAME <YOUR@EMAIL.com>
2018-04-26 22:27:12 +0700

    Chap 7

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "static_pages.base_title"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(params[:id])
+    return if @user
+    flash[:success] = t "static_pages.not_found"
+    redirect_to root_path
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :gender,
+     :password, :password_confirmation)
+  end
+end
