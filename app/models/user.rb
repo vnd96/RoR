77bc3a88b5c335e5556ca631020f117a10957744
YOUR NAME <YOUR@EMAIL.com>
2018-05-01 17:44:47 +0700

    Chap 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,10 @@ class User < ApplicationRecord
   def email_dowcase
     email.downcase!
   end
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
